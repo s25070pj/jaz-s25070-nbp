0f5961cabb2c7e26486ead05b3186c1edab1148d
brash <s25070@pjwstk.edu.pl>
2023-06-24 15:09:15 +0200

    initial commit

---
+++ src/test/java/com/example/jazs25070nbp/JazS25070NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs25070nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS25070NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
